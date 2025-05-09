e1d06fc90ea517954d2b1c74f2b718cbb11fa1ce
JOLLYSTRIX <reduxdevil@gmail.com>
2025-05-09 17:09:51 +0300

    refactor(service_provider): consistently use arrow functions in singleton method

--- src/RemnawaveServiceProvider.php
+++ src/RemnawaveServiceProvider.php
@@ -9,42 +9,42 @@ use Laravel\Lumen\Application as LumenApplication;
 
 class RemnawaveServiceProvider extends BaseServiceProvider
 {
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register(): void
-    {
-        $this->mergeConfigFrom($this->configPath(), 'remnawave');
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register(): void
+	{
+		$this->mergeConfigFrom($this->configPath(), 'remnawave');
 
-        $this->app->singleton(RemnawaveClient::class, function ($app) {
-            $config = $this->app['config']->get('remnawave');
-            return new RemnawaveClient($config);
-        });
-    }
+		$this->app->singleton(RemnawaveClient::class, function () {
+			$config = $this->app['config']->get('remnawave');
+			return new RemnawaveClient($config);
+		});
+	}
 
-    /**
-     * Register the config for publishing
-     *
-     */
-    public function boot()
-    {
-        if ($this->app instanceof LaravelApplication && $this->app->runningInConsole()) {
-            $this->publishes([$this->configPath() => config_path('remnawave.php')], 'remnawave');
-        } elseif ($this->app instanceof LumenApplication) {
-            $this->app->configure('remnawave');
-        }
-    }
+	/**
+	 * Register the config for publishing
+	 *
+	 */
+	public function boot()
+	{
+		if ($this->app instanceof LaravelApplication && $this->app->runningInConsole()) {
+			$this->publishes([$this->configPath() => config_path('remnawave.php')], 'remnawave');
+		} elseif ($this->app instanceof LumenApplication) {
+			$this->app->configure('remnawave');
+		}
+	}
 
-    /**
-     * Set the config path
-     *
-     * @return string
-     */
-    protected function configPath()
-    {
-        return __DIR__ . '/../config/remnawave.php';
-    }
+	/**
+	 * Set the config path
+	 *
+	 * @return string
+	 */
+	protected function configPath()
+	{
+		return __DIR__ . '/../config/remnawave.php';
+	}
 
 }
